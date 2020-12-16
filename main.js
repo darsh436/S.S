d678e41facf87f60b1b8b3eb045490f4c17c8403
darsh436 <69808309+darsh436@users.noreply.github.com>
2020-12-16 16:17:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function ab(){
+    window.location = "1.html"
+}
+function abc(){
+    window.location = "2.html"
+}
+function abcd(){
+    window.location = "3.html"
+}
+function abcde(){
+    window.location = "4.html"
+}
+function abcdef(){
+    window.location = "5.html"
+}
+function abcdefg(){
+    window.location = "6.html"
+}
+function abcdefgh(){
+    window.location = "7.html"
+}
+function abcdefghi(){
+    window.location = "8.html"
+}
+function abca(){
+    window.location = "9.html"
+}
+function abcb(){
+    window.location = "10.html"
+}
